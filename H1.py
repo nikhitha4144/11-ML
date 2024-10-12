80a2003553e8cd77c9329aa99df5bc9d3311edb7
Nikhitha B <nikhithabuditha@gmail.com>
2024-10-12 11:49:38 +0530

    modified

--- H1.py
+++ H1.py
@@ -6,8 +6,8 @@ model = joblib.load("Rent.pkl")
 st.title("House Rent Prediction Model")
 Size=st.number_input("Please enter Size",min_value =100.0, step = 50.0) # 22
 BHK = st.selectbox("Sex",["1","2","3","4","5"]) # Male
-Furnishing Status=st.selectbox("Furnishing Status",["0","1","2"]) # Yes
-Tenant Preferred=st.selectbox("Tenant Preferred	",["0","1","2"]) # Sun
+Furnishing_Status = st.selectbox("Furnishing Status",["1","0","2"]) # Yes
+Tenant_Preferred = st.selectbox("Tenant Preferred	",["0","1","2"]) # Sun
 Bedroom=st.selectbox("Bedroom",["1","2","3","4","5","6","7","8","9","10"]) # Dinner
 
 # gender = st.selectbox("Sex",["Male","Female"]) # Male
@@ -15,11 +15,7 @@ Bedroom=st.selectbox("Bedroom",["1","2","3","4","5","6","7","8","9","10"]) # Din
 # day=st.selectbox("day",["Thur","Fri","Sat","Sun"]) # Sun
 # time=st.selectbox("time",["Lunch","Dinner"]) # Dinner
 
-gender_value = 0 if gender == "Male" else 1
-smoker_value = 0 if smoker=="Yes" else 1
-day_value = {"Thur":0, "Fri":1,"Sat":2,"Sun":3}[day]
-time_value = 0 if time =="Lunch" else 1
-
+g
 if st.button("Predict"):
-    result = model.predict([[total_bill,gender_value,smoker_value,day_value,time_value,size]])
-    st.write(f"The predicted tip amount is ${result[0]:.2f}")
+    result = model.predict([[Size,Furnishing_Status,Tenant_Preferred,Bedroom,]])
+    st.write(f"The predicted House Rent is ${result[0]:.2f}")
