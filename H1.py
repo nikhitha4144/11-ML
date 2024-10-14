2bc0b269f57a9312e00e60503c06e93aea9acd19
Nikhitha B <nikhithabuditha@gmail.com>
2024-10-14 15:03:23 +0530

    first commit

--- H1.py
+++ H1.py
@@ -5,19 +5,11 @@ model = joblib.load("Rent.pkl")
 
 st.title("House Rent Prediction Model")
 st.image("dataset-cover.jpg")
-Size=st.number_input("Please enter Size",min_value =100, step = 50) # 22
-BHK = st.selectbox("BHK",["1","2","3","4","5"]) # Male
-Furnishing_Status = st.selectbox("Furnishing Status",["Unfurnished","Semi_Furnished","Furnished"]) # Yes
-Tenant_Preferred = st.selectbox("Tenant Preferred	",["Bachelors","Bachelors/Family","Family"]) # Sun
+Size=st.number_input("Please enter Size",min_value =100, step = 50) 
+BHK = st.selectbox("BHK",["1","2","3","4","5"])
+Furnishing_Status = st.selectbox("Furnishing Status",["Unfurnished","Semi_Furnished","Furnished"])
+Tenant_Preferred = st.selectbox("Tenant Preferred	",["Bachelors","Bachelors/Family","Family"])
 
-# gender = st.selectbox("Sex",["Male","Female"]) # Male
-# smoker=st.selectbox("smoker",["Yes","No"]) # Yes
-# day=st.selectbox("day",["Thur","Fri","Sat","Sun"]) # Sun
-# time=st.selectbox("time",["Lunch","Dinner"]) # Dinner
-
-
-
-# Encode Furnishing_Status
 if Furnishing_Status == "Unfurnished":
     Furnishing_Status = 0
 elif Furnishing_Status == "Semi-Furnished":
@@ -25,12 +17,11 @@ elif Furnishing_Status == "Semi-Furnished":
 else:  # Furnished
     Furnishing_Status = 2
 
-# Encode Tenant_Preferred
 if Tenant_Preferred == "Family":
     Tenant_Preferred = 0
 elif Tenant_Preferred == "Bachelor":
     Tenant_Preferred = 1
-else:  # Company
+else:  
     Tenant_Preferred = 2
 
 Size = float(Size)
